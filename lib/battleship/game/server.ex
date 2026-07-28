fb2d3df5a3c67b3e0793167f88c3eeda1a9dbd00
ffutryk <ffutryk52@gmail.com>
2026-07-28 07:32:11 +0000

    feat: add placement phase initialization and timer

--- lib/battleship/game/server.ex
+++ lib/battleship/game/server.ex
@@ -4,6 +4,9 @@ defmodule Battleship.Game.Server do
   alias Battleship.Game.State
   alias Battleship.Game.Registry, as: GameRegistry
 
+  @placement_seconds 30
+  @placement_timeout :timer.seconds(@placement_seconds)
+
   def start_link({game_id, players}) do
     GenServer.start_link(__MODULE__, {game_id, players}, name: GameRegistry.via_tuple(game_id))
   end
@@ -11,8 +14,18 @@ defmodule Battleship.Game.Server do
   def get_state(game_id), do: GenServer.call(GameRegistry.via_tuple(game_id), :get_state)
 
   @impl true
-  def init({game_id, players}), do: {:ok, %State{id: game_id, players: players}}
+  def init({game_id, players}) do
+    timer_ref = Process.send_after(self(), :placement_timeout, @placement_timeout)
+    placement_deadline = System.monotonic_time(:second) + @placement_seconds
+
+    state = State.init(game_id, players, timer_ref, placement_deadline)
+
+    {:ok, state}
+  end
 
   @impl true
   def handle_call(:get_state, _from, state), do: {:reply, state, state}
+
+  @impl true
+  def handle_info(:placement_timeout, state), do: {:noreply, state}
 end
--- lib/battleship/game/state.ex
+++ lib/battleship/game/state.ex
@@ -1,3 +1,31 @@
 defmodule Battleship.Game.State do
-  defstruct [:id, :players]
+  defstruct [
+    :id,
+    :players,
+    :phase,
+    :timer_ref,
+    :placement_deadline,
+    :boards
+  ]
+
+  def init(id, players, timer_ref, placement_deadline) do
+    boards = Enum.into(players, %{}, fn player -> {player.id, %{}} end)
+
+    %__MODULE__{
+      id: id,
+      players: players,
+      timer_ref: timer_ref,
+      placement_deadline: placement_deadline,
+      phase: :placement,
+      boards: boards
+    }
+  end
+
+  def next_phase(%__MODULE__{phase: :placement} = state) do
+    %__MODULE__{state | phase: :battle, timer_ref: nil, placement_deadline: nil}
+  end
+
+  def next_phase(%__MODULE__{phase: :battle} = state) do
+    %__MODULE__{state | phase: :game_over}
+  end
 end
