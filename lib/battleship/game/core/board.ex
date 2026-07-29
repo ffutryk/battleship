4a781c31f69708ece1c5769e376022dbbb38e578
ffutryk <ffutryk52@gmail.com>
2026-07-29 05:01:42 +0000

    feat: track fleet availability with a list, add ship shape and size validations

--- lib/battleship/game/core/board.ex
+++ lib/battleship/game/core/board.ex
@@ -1,20 +1,40 @@
 defmodule Battleship.Game.Core.Board do
-  defstruct ships: [], shots: MapSet.new()
+  @size 9
+  @fleet [2, 2, 2, 2, 2]
+
+  defstruct ships: [], shots: MapSet.new(), available_ships: @fleet
 
   alias Battleship.Game.Core.Ship
 
-  @size 9
-  @ships_required 5
+  def place_ship(%__MODULE__{available_ships: []}, _coords) do
+    {:error, :max_ships_reached}
+  end
 
   def place_ship(board, coords) do
     ship = %Ship{coordinates: MapSet.new(coords)}
+    ship_length = length(coords)
 
     with :ok <- validate_not_empty(coords),
          :ok <- validate_bounds(coords),
+         {:ok, remaining_ships} <- validate_and_pop_ship(board.available_ships, ship_length),
+         :ok <- validate_straight_line(coords),
          :ok <- validate_no_overlap(board, ship) do
-      {:ok, %{board | ships: [ship | board.ships]}}
+      updated_board = %{
+        board
+        | ships: [ship | board.ships],
+          available_ships: remaining_ships
+      }
+
+      {:ok, updated_board}
     end
   end
+
+  def all_sunken?(%__MODULE__{ships: ships, shots: shots}) do
+    Enum.all?(ships, &Ship.sunk?(&1, shots))
+  end
+
+  def ready?(%__MODULE__{available_ships: []}), do: true
+  def ready?(%__MODULE__{}), do: false
 
   defp validate_not_empty(coords) do
     if coords == [], do: {:error, :empty_ship}, else: :ok
@@ -28,8 +48,20 @@ defmodule Battleship.Game.Core.Board do
     if overlaps?(board, ship), do: {:error, :overlapping}, else: :ok
   end
 
-  def all_sunken?(%__MODULE__{ships: ships, shots: shots}) do
-    Enum.all?(ships, &Ship.sunk?(&1, shots))
+  defp validate_and_pop_ship(available_ships, ship_length) do
+    if ship_length in available_ships do
+      {:ok, List.delete(available_ships, ship_length)}
+    else
+      {:error, :invalid_or_unneeded_ship_size}
+    end
+  end
+
+  defp validate_straight_line(coords) do
+    case Enum.sort(coords) do
+      [{r1, c1}, {r2, c2}] when r1 == r2 and c2 == c1 + 1 -> :ok
+      [{r1, c1}, {r2, c2}] when c1 == c2 and r2 == r1 + 1 -> :ok
+      _ -> {:error, :invalid_ship_shape}
+    end
   end
 
   defp within_bounds?({row, col}) do
@@ -43,6 +75,4 @@ defmodule Battleship.Game.Core.Board do
       not MapSet.disjoint?(ship.coordinates, coords)
     end)
   end
-
-  def ready?(%__MODULE__{ships: ships}), do: length(ships) == @ships_required
 end
