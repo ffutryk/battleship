2884ee5e47942a8ea4a16bc73753e4c95eecd31d
ffutryk <ffutryk52@gmail.com>
2026-07-29 05:16:40 +0000

    feat: add ship placement and confirmation to game server

--- lib/battleship/game/server.ex
+++ lib/battleship/game/server.ex
@@ -2,6 +2,7 @@ defmodule Battleship.Game.Server do
   use GenServer
 
   alias Battleship.Game.State
+  alias Battleship.Game.Core.Board
   alias Battleship.Game.Registry, as: GameRegistry
 
   @placement_seconds 30
@@ -12,6 +13,14 @@ defmodule Battleship.Game.Server do
   end
 
   def get_state(game_id), do: GenServer.call(GameRegistry.via_tuple(game_id), :get_state)
+
+  def place_ship(game_id, player_id, coords) do
+    GenServer.call(GameRegistry.via_tuple(game_id), {:place_ship, player_id, coords})
+  end
+
+  def confirm_placement(game_id, player_id) do
+    GenServer.call(GameRegistry.via_tuple(game_id), {:confirm_placement, player_id})
+  end
 
   @impl true
   def init({game_id, players}) do
@@ -27,5 +36,58 @@ defmodule Battleship.Game.Server do
   def handle_call(:get_state, _from, state), do: {:reply, state, state}
 
   @impl true
+  def handle_call({:place_ship, player_id, coords}, _from, %State{phase: :placement} = state) do
+    board = Map.fetch!(state.boards, player_id)
+
+    case Board.place_ship(board, coords) do
+      {:ok, updated_board} ->
+        new_state = %{state | boards: Map.put(state.boards, player_id, updated_board)}
+        {:reply, {:ok, updated_board}, new_state}
+
+      error ->
+        {:reply, error, state}
+    end
+  end
+
+  def handle_call({:place_ship, _, _}, _from, state) do
+    {:reply, {:error, :not_placement_phase}, state}
+  end
+
+  def handle_call({:confirm_placement, player_id}, _from, %State{phase: :placement} = state) do
+    board = Map.fetch!(state.boards, player_id)
+
+    if Board.ready?(board) do
+      new_state = State.mark_ready(state, player_id)
+
+      if State.all_ready?(new_state) do
+        {:reply, :ok, start_battle(new_state)}
+      else
+        {:reply, :ok, new_state}
+      end
+    else
+      {:reply, {:error, :incomplete_placement}, state}
+    end
+  end
+
+  def handle_call({:confirm_placement, _player_id}, _from, state) do
+    {:reply, {:error, :not_placement_phase}, state}
+  end
+
+  @impl true
+  def handle_info(:placement_timeout, %State{phase: :placement} = state) do
+    {:noreply, start_battle(state)}
+  end
+
+  @impl true
   def handle_info(:placement_timeout, state), do: {:noreply, state}
+
+  defp start_battle(state) do
+    if state.timer_ref, do: Process.cancel_timer(state.timer_ref)
+
+    boards = Map.new(state.boards, fn {id, board} -> {id, Board.fill_random(board)} end)
+
+    Phoenix.PubSub.broadcast(Battleship.PubSub, "game:#{state.id}", {:phase_changed, :battle})
+
+    %{state | boards: boards, timer_ref: nil} |> State.next_phase()
+  end
 end
