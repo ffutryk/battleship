fff281fd6468c4166edfe08cb0062a4fc7d1d1f3
ffutryk <ffutryk52@gmail.com>
2026-07-31 05:59:43 +0000

    feat: build ship sprite paths, add dynamic generation by stretching midparts

---
+++ lib/battleship_web/game/ship_sprites.ex
@@ -0,0 +1,45 @@
+defmodule BattleshipWeb.Game.ShipSprites do
+  use BattleshipWeb, :verified_routes
+
+  alias Battleship.Game.Core.Ship
+
+  @sprite_files %{
+    2 => ["destroyer_bow.png", "destroyer_stern.png"]
+  }
+
+  def for_length(length) do
+    if Ship.valid_length?(length) do
+      @sprite_files
+      |> Map.fetch!(length)
+      |> stretch(length)
+      |> Enum.map(&path/1)
+    else
+      raise "Unsupported ship length: #{length}"
+    end
+  end
+
+  defp path(filename), do: ~p"/images/ships/#{filename}"
+
+  defp stretch(sprites, 1), do: [List.first(sprites)]
+
+  defp stretch(sprites, length) when length >= 2 do
+    bow = List.first(sprites)
+    stern = List.last(sprites)
+    mids_needed = length - 2
+
+    mids =
+      if mids_needed <= 0 do
+        []
+      else
+        mid_pool =
+          case Enum.slice(sprites, 1..-2//1) do
+            [] -> [bow]
+            pool -> pool
+          end
+
+        mid_pool |> Stream.cycle() |> Enum.take(mids_needed)
+      end
+
+    [bow] ++ mids ++ [stern]
+  end
+end
