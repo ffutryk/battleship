a2adbb7d6d553fee65ad4230b6c903f7fb362e6f
ffutryk <ffutryk52@gmail.com>
2026-08-03 23:36:17 +0000

    feat: tie bot lifecycle to its game via MatchSupervisor

--- lib/battleship/game/supervisor.ex
+++ lib/battleship/game/supervisor.ex
@@ -1,17 +1,17 @@
 defmodule Battleship.Game.Supervisor do
   use DynamicSupervisor
 
-  alias Battleship.Game.Server
+  alias Battleship.Game.MatchSupervisor
 
   def start_link(_arg), do: DynamicSupervisor.start_link(__MODULE__, :ok, name: __MODULE__)
 
   @impl true
   def init(:ok), do: DynamicSupervisor.init(strategy: :one_for_one)
 
-  def start_game(game_id, player_ids) do
+  def start_game(game_id, player_ids, bot_id \\ nil) do
     child_spec =
       Supervisor.child_spec(
-        {Server, {game_id, player_ids}},
+        {MatchSupervisor, {game_id, player_ids, bot_id}},
         restart: :transient
       )
 
